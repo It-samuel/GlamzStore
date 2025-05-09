f4e87f824a407e1804fb8d16787f1fa2032a5578
Dr Xam <samuelhappiness360@gmail.com>
2025-05-09 13:55:35 +0100

    header and footer config

---
+++ components/ui/shared/header/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { ShoppingCart, UserIcon } from 'lucide-react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { APP_NAME } from '@/lib/constants'
+
+
+export default function Header() {
+  return (
+    <header className= 'w-full border-b'>
+        <div className="wrapper flex-between">
+            <div className="flex-start">
+                <Link href="/" className="flex-start">
+                    <Image src='/images/favicon.png' alt={`${APP_NAME} logo`}
+                    height={48}
+                    width={48}
+                    priority={true}/>
+                    <span className="hidden font-bold text-2xl ml-3 lg:block">
+                        {APP_NAME}
+                    </span>
+                </Link>
+            </div>
+            <div className="space-x-2">
+                <Button asChild variant="ghost" >
+                    <Link href="/cart">
+                        <ShoppingCart /> Cart
+                    </Link>
+
+                </Button>
+                <Button asChild variant="ghost" >
+                    <Link href="/sign-in">
+                        <UserIcon /> Sign In
+                    </Link>
+
+                </Button>
+            </div>
+        </div>
+    </header>
+  )
+}
