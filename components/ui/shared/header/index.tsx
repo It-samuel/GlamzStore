f19ce41fc0561dd79cb0d38f1ad12eb7f9287783
Dr Xam <samuelhappiness360@gmail.com>
2025-05-10 15:23:32 +0100

    added toggle mode

--- components/ui/shared/header/index.tsx
+++ components/ui/shared/header/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
+import ModeToggle from "@/components/ui/shared/header/mode-toggle"
 
 
 export default function Header() {
@@ -21,14 +22,16 @@ export default function Header() {
                     </span>
                 </Link>
             </div>
+            
             <div className="space-x-2">
+                 <ModeToggle />
                 <Button asChild variant="ghost" >
                     <Link href="/cart">
                         <ShoppingCart /> Cart
                     </Link>
 
                 </Button>
-                <Button asChild variant="ghost" >
+                <Button asChild >
                     <Link href="/sign-in">
                         <UserIcon /> Sign In
                     </Link>
---
+++ components/ui/shared/header/mode-toggle.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuCheckboxItem,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../../button";
+import { Sun, Moon, SunMoon } from "lucide-react";
+
+export default function Toggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="focus-visible:ring-0 focus:ring-offset-0"
+        >
+          {theme === "system" ? (
+            <SunMoon />
+          ) : theme === "dark" ? (
+            <Moon />
+          ) : (
+            <Sun />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={theme === "system"}
+          onClick={() => setTheme("system")}
+        >
+          System
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === "dark"}
+          onClick={() => setTheme("dark")}
+        >
+          Dark
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === "light"}
+          onClick={() => setTheme("light")}
+        >
+          Light
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
