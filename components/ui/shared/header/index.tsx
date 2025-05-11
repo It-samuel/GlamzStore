c4b927f5e2f3987507da1be15ec6dd5450439344
Dr Xam <samuelhappiness360@gmail.com>
2025-05-11 15:02:08 +0100

    added reponsiveness

--- components/ui/shared/header/index.tsx
+++ components/ui/shared/header/index.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
 import ModeToggle from "@/components/ui/shared/header/mode-toggle"
+import Menu from "@/components/ui/shared/header/menu";
 
 
 export default function Header() {
@@ -23,21 +24,7 @@ export default function Header() {
                 </Link>
             </div>
             
-            <div className="space-x-2">
-                 <ModeToggle />
-                <Button asChild variant="ghost" >
-                    <Link href="/cart">
-                        <ShoppingCart /> Cart
-                    </Link>
-
-                </Button>
-                <Button asChild >
-                    <Link href="/sign-in">
-                        <UserIcon /> Sign In
-                    </Link>
-
-                </Button>
-            </div>
+            <Menu />
         </div>
     </header>
   )
