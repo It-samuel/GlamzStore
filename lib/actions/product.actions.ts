3215c2d7feb3741c0777e015d1b4bfbc9dc61110
Dr Xam <samuelhappiness360@gmail.com>
2025-05-12 13:59:40 +0100

    prisma client error fixed

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,19 @@
+'use server'
+import { PrismaClient} from '@prisma/client'
+import { convertPrismaObject } from '@/lib/utils';
+
+
+// Get latest products
+
+export async function getLatestProducts() {
+    const prisma = new PrismaClient();
+
+    const data = await prisma.product.findMany({
+        take: 4,
+        orderBy: {
+            createdAt: 'desc',
+        }
+    });
+
+    return convertPrismaObject(data);
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+// Convert a prisma object to a plain object
+export function convertPrismaObject<T>(value: T): T {
+  return JSON.parse(JSON.stringify(value)) as T;
+} 
