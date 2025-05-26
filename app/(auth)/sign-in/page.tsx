64c859b143b7957f9c86d5b603dcf1f4c948b48c
Dr Xam <samuelhappiness360@gmail.com>
2025-05-26 14:24:01 +0100

    sing in page component

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,33 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
+
+
+export const metadate: Metadata = {
+    title: 'Sign In'
+}
+
+export default function SigbInPage() {
+  return (
+    <div className="w-full max-w-md mx-auto">
+        <Card>
+            <CardHeader className="space-y-4">
+                <Link href='/' className="flex-center">
+                    <Image src='/images/favicon.png' alt="logo" width={100} height={100} priority={true}/>
+                </Link>
+                <CardTitle className="text-center"> Sign In</CardTitle>
+                <CardDescription className="text-center">
+                    Sign in to your account
+
+                </CardDescription>
+            </CardHeader>
+
+            <CardContent className="space-y-4">
+
+            </CardContent>
+        </Card>
+
+    </div>
+  )
+}
