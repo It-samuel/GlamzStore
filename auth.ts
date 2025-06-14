4eec17c0a49d7af3d5e61ce0a90bcaebb2d7ec06
Dr Xam <samuelhappiness360@gmail.com>
2025-06-14 01:32:08 +0100

    checkout steps

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import {prisma} from '@/db/prisma'
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { cookies } from 'next/headers';
 
 export const config = {
     pages: {
@@ -77,6 +78,7 @@ export const config = {
             
             // Assign user field to token
             if (user){
+                token.id = user.id;
                 token.role = user.role;
 
                 // If user has no name then use the email
@@ -88,6 +90,31 @@ export const config = {
                         where: {id: user.id},
                         data: {name: token.name}
                     })
+                }
+
+                if (trigger === 'signIn' || trigger === 'signUp') {
+                    const cookiesObject = await cookies();
+                    const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+                    if(sessionCartId) {
+                        const sessionCart = await prisma.cart.findFirst({
+                            where: {sessionCartId}
+                        })
+
+                        if (sessionCart) {
+                            // Delete current user cart
+                            await prisma.cart.deleteMany({
+                                where: { userId: user.id}
+                            })
+
+                            // Assign new cart
+                            await prisma.cart.update({
+                                where: {id: sessionCart.id},
+                                data: {userId: user.id},
+                            })
+                        }
+                    }
+
                 }
             }
 
